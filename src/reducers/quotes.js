4ad575f4a9a14f781196a2833aa8489e214f0bd3
katycarr <khcarr9@gmail.com>
2018-03-31 14:58:14 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return [...state].filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      let quote = [...state].find(q => q.id === action.quoteId)
+      quote.votes ++
+      return [...state]
+    case 'DOWNVOTE_QUOTE':
+      let toDecrease = [...state].find(q => q.id === action.quoteId)
+      if (toDecrease.votes > 0) {
+        toDecrease.votes --
+      }
+      return [...state]
+    default:
+      return state
+  }
 }
